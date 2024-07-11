4d35001e4ce2b0715b6ea1674d2b1bfdd42b181a
Harshit <kartiklohani2004@gmail.com>
2024-07-11 12:25:20 +0530

    Changes made

--- bcics/Code/main.py
+++ bcics/Code/main.py
@@ -1,1 +1,12 @@
 # Model Training and evaluation
+from pipeline import X, y, ct
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import mean_absolute_error, roc_curve
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.01, random_state=42)
+
+ct.fit(X_train)
+Xnew = ct.transform(X_train)
+
--- bcics/Code/pipeline.py
+++ bcics/Code/pipeline.py
@@ -1,21 +1,25 @@
 import numpy as np
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
 from custom_transformers import custom_LabelEncoder, ColumnDropper
-from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.ensemble import RandomForestClassifier
 
 rd = "D:/My Competitions/Binary Classification of Insurance Cross Selling/Data"
 
 train_data = pd.read_csv(os.path.join(rd, "train/train.csv"))
 
+cd = ColumnDropper()
+cd.fit(train_data)
+train_data = cd.transform(train_data)
+
+X = train_data.drop(columns=["Response"], axis = 1)
+y = train_data["Response"]
+
 # Sorting the columns into numeric and object data
-objCols = train_data.select_dtypes(include = object)
-intCols = train_data.select_dtypes(include = np.number)
+objCols = X.select_dtypes(include = object).columns
+intCols = X.select_dtypes(include = np.number).columns
 
 objSteps = [("label encoder", custom_LabelEncoder())] # For the obj columns
 intSteps = [("scaler", StandardScaler())] # For the integer columns
@@ -24,13 +28,5 @@ pipe1 = Pipeline(steps=objSteps)
 pipe2 = Pipeline(steps=intSteps)
 
 ct = ColumnTransformer(transformers=[("obj cols", pipe1, objCols), ("int cols", pipe2, intCols)])
-
-finalPipeline = make_pipeline(ColumnDropper, ct)
-
-X = train_data.drop(columns=["Response"], axis = 1)
-y = train_data["Response"]
-
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.01, random_state=42)
-
 
 
